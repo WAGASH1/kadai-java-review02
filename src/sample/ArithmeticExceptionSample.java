1be7ed989a97afa815b3e377f999b4169c098821
WAGASH1 <takuannshoui@icloud.com>
2023-11-12 09:56:43 +0900

    Javaプログラミング入門学習終了

---
+++ src/sample/ArithmeticExceptionSample.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class ArithmeticExceptionSample {
+
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 10;
+        int result;
+        result = sumMethod2(num1 , num2);
+        System.out.println(result + "Method2");
+
+    }
+
+    public static int sumMethod2(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+
+}
